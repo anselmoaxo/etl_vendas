148a85b09935f09fa40c8d5a120d9dde154e2df3
anselmo.axo <anselmo.cstecnologia@gmail.com>
2025-04-01 11:44:43 -0300

    add airflow

--- src/extract.py
+++ src/extract.py
@@ -1,33 +1,38 @@
 from conection import conexao_postgresql
+import pandas as pd
+import os
 
 def extract_data():
-    """Extrai dados do banco de dados PostgreSQL."""
+    """Extrai os 10 primeiros registros de cada tabela e salva em CSV no diretório ../data."""
     conn = conexao_postgresql()
     if conn is None:
         raise ValueError("Falha ao criar a conexão com o banco.")
     
     table_names = ['veiculos', 'estados', 'cidades', 'concessionarias', 
                    'vendedores', 'clientes', 'vendas']
-    max_ids = {}
+    
+    # Definir o diretório de saída relativo ao script
+    output_dir = "/home/anselmo/etl_vendas/data"
+    # Criar o diretório se não existir
+    os.makedirs(output_dir, exist_ok=True)
     
     try:
-        # Criar um cursor para executar as queries
         with conn.cursor() as cursor:
             for table_name in table_names:
-                try:
-                    query = f"SELECT MAX(id_{table_name}) FROM {table_name}"
-                    cursor.execute(query)
-                    result = cursor.fetchone()[0]  # Pega o primeiro valor da linha
-                    max_ids[table_name] = result if result is not None else 0
-                except Exception as e:
-                    print(f"Erro ao processar {table_name}: {e}")
-                    max_ids[table_name] = 0
-        conn.commit()  # Não necessário aqui, mas boa prática
+                query = f"SELECT * FROM {table_name} "
+                cursor.execute(query)
+                # Ler os dados diretamente em um DataFrame
+                df = pd.read_sql_query(query, conn)
+                # Caminho completo para o arquivo CSV
+                output_file = os.path.join(output_dir, f"{table_name}.csv")
+                # Salvar o DataFrame como CSV
+                df.to_csv(output_file, index=False)
+        conn.commit()  
+    except Exception as e:
+        print(f"❌ Erro durante a extração: {e}")
     finally:
         conn.close()  # Garante que a conexão seja fechada
-    
-    return max_ids  
+        
 
 if __name__ == "__main__":
-    result = extract_data()
-    print(f"Resultado final: {result}")
+    extract_data()
